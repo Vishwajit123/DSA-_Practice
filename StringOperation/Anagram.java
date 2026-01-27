42caeee9a58553d35085ae8fab8c8e09d5121b7e
Vishwajit <shindevishwajit777@gmail.com>
2026-01-27 14:55:36 +0530

    anagram

---
+++ StringOperation/Anagram.java
@@ -0,0 +1,33 @@
+public class Anagram {
+    public static boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) {
+
+            return false;
+        }
+
+        int[] count =new int[26];
+
+        for(int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for(int c : count) {
+            if(c != 0)
+                return false;
+        }
+
+        return true;
+    }
+
+     public static void main(String[] args) {
+        String s = "listen";
+        String t = "silent";
+
+        if (isAnagram(s, t))
+            System.out.println("Anagram ");
+        else
+            System.out.println("Not Anagram ");
+    }
+}
