333ad8b82c640eafb136c9f2329f178be2304725
Vishwajit <shindevishwajit777@gmail.com>
2026-01-27 15:25:50 +0530

    add  variable

---
+++ Practice/variable.java
@@ -0,0 +1,39 @@
+public class variable {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+
+         short a = 10;
+        short b = 20;
+
+        
+
+        char a = 'a';
+
+        char b = 'b';
+
+        
+
+        long a = 487687368763487l;
+        long b = 892378937897893l;
+
+        float a = 3.44f;
+        float b = 7.44f;
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        int ans = a % b;
+
+        boolean ans = isTrue + isFalse;
+
+        String a = "Vishwajit";
+
+        String b = "Shinde";
+
+        String ans = a % b;
+
+        System.out.println(ans);
+
+    }
+}
