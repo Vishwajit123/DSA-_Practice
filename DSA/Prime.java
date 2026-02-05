36b2e33da3182ebc61494666ef94588fae7a9890
Vishwajit <shindevishwajit777@gmail.com>
2026-02-05 14:50:20 +0530

    Added Prime program

---
+++ DSA/Prime.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();   
+
+        int count = 0;
+
+        System.out.print("Prime numbers: ");
+
+        for (int j = 2; j < n; j++) {
+            if (isPrime(j)) {
+                System.out.print(j + " ");  
+                count++;
+            }
+        }
+
+        System.out.println("\nTotal primes: " + count); 
+    }
+
+    public static boolean isPrime(int num) {
+        if (num < 2) return false;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
