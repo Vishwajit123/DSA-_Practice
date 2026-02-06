5edc83afba3c580cf7d490084d40c81a6d7e07a3
Vishwajit <shindevishwajit777@gmail.com>
2026-02-06 09:38:58 +0530

    some updated code

--- DSA/Feb5.java
+++ DSA/Feb5.java
@@ -1,11 +1,20 @@
+// ------------------------------------ rotate a 2D Matrix by 90 Degrees ------------------------------------
+
 import java.util.*;
 
 class Feb5 {
 
+                              
+
     public void rotate(int[][] matrix) {
         int n = matrix.length;
+                            
+        
+                              // 1 2 3  
+                              // 4 5 6
+                              // 7 8 9
+                             
 
-       
         for(int i = 0; i < n; i++) {
             for(int j = i; j < n; j++) {
                 int temp = matrix[i][j];
@@ -14,6 +23,10 @@ class Feb5 {
             }
         }
 
+                            // 1 4 7
+                            // 2 5 8 
+                            // 3 6 9  
+        
         
         for(int i = 0; i < n; i++) {
             int left = 0;
@@ -28,6 +41,10 @@ class Feb5 {
             }
         }
     }
+                    // reverse columns 
+                                // 7 4 1
+                                // 8 5 2
+                                // 9 6 3
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -44,7 +61,7 @@ class Feb5 {
             }
         }
 
-        Feb5 obj = new Feb5();1
+        Feb5 obj = new Feb5();
         obj.rotate(matrix);
 
         System.out.println("Rotated Matrix:");
