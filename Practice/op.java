89bacc06a901f194f939346dc787ec290f020caf
Vishwajit <shindevishwajit777@gmail.com>
2026-02-11 22:24:34 +0530

    using the operator

---
+++ Practice/op.java
@@ -0,0 +1,17 @@
+class op{
+    public  static void main(String[] args){
+        int  a = 6;
+        // a = *4;
+        // a = -0;
+    //    int  b = a++ + ++a;
+    //    int c = b++;
+        // System.out.println(++b +a +c);  
+
+        // System.out.println(a *= a -= 10);
+
+        // System.out.println(a *= a += a -= 4);
+
+        System.out.println(a /= a *= a += 10);
+
+    }
+}
