4ab0441ece07f0ddc37633bd635cd9ebc85bf4b9
Vishwajit <shindevishwajit777@gmail.com>
2026-02-12 11:02:52 +0530

    to find out tthe majority element

---
+++ DSA/Feb12.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int count = 0, candidate = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        System.out.println("Majority Element: " + candidate);
+    }
+}
