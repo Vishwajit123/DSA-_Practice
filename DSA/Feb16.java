bc43e8a802900e3d79f2e98eeb789a0006b2c88e
Vishwajit <shindevishwajit777@gmail.com>
2026-02-16 11:50:14 +0530

    Array Partition

---
+++ DSA/Feb16.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class ArrayPartition {
+
+    public static int maxSumOfMinPairs(int[] nums) {
+
+        
+        Arrays.sort(nums);
+
+        int sum = 0;
+
+        
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter total number of elements (must be even): ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = maxSumOfMinPairs(nums);
+
+        System.out.println("Maximum Sum = " + result);
+    }
+}
