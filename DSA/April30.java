119fbd9edf240fd6dce18c72ca2b7b1713ab8605
Vishwajit <shindevishwajit777@gmail.com>
2026-04-30 17:13:26 +0530

    .

---
+++ DSA/April30.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class April30 {
+    public static boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();  
+
+        System.out.println(canWinNim(n));
+
+        sc.close();
+    }
+}
