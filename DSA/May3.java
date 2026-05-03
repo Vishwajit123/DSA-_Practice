db541058ac3dbb2cf344faa9864df161120f3d03
Vishwajit <shindevishwajit777@gmail.com>
2026-05-03 15:21:43 +0530

    .

---
+++ DSA/May3.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class May3 {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] > target) col--;
+            else row++;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int target = sc.nextInt();
+
+        System.out.println(searchMatrix(matrix, target));
+    }
+}
